09f5c0ee416775ead5065b3abdaf1a799740d607
Facundo Rodriguez <facundonrodriguez@gmail.com>
2025-01-22 18:04:28 -0300

    feat: update dependencies to latest versions and improve database URL handling for production

--- src-tauri/src/db/sqlite.rs
+++ src-tauri/src/db/sqlite.rs
@@ -1,23 +1,36 @@
 use diesel::prelude::*;
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use dirs::data_dir;
 use dotenvy::dotenv;
 use std::{env, error::Error};
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
+fn get_dev_db_url() -> String {
+    env::var("DATABASE_URL").expect("DATABASE_URL must be set")
+}
+
+fn get_prod_db_url() -> String {
+    let mut data_path = data_dir().unwrap();
+    data_path.push("LaburAR");
+    std::fs::create_dir_all(&data_path).expect("Failed to create LaburAR directory");
+
+    let path = data_path.join("database.db");
+    if !path.exists() {
+        std::fs::File::create(&path).expect("Failed to create database file");
+    }
+    path.to_str()
+        .expect("Failed to convert path to string")
+        .to_string()
+}
+
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    let database_url: String = if cfg!(debug_assertions) {
-        // Development environment
-        env::var("DATABASE_URL").expect("DATABASE_URL must be set")
+    let database_url: String = if cfg!(dev) {
+        get_dev_db_url()
     } else {
-        // Production environment
-        let mut path = std::path::PathBuf::from(tauri::path::BaseDirectory::Data.variable());
-        path.push("database.db");
-        path.to_str()
-            .expect("Failed to convert path to string")
-            .to_string()
+        get_prod_db_url()
     };
 
     SqliteConnection::establish(&database_url)
