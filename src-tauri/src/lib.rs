4043b1315cccf33a396c5c5fc06a67ade152dbd9
Facundo Rodriguez <facundonrodriguez@gmail.com>
2025-01-31 16:19:07 -0300

    feat: add updater plug

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -56,6 +56,7 @@ use db::sqlite::run_migrations;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .setup(|_app| {
             // Call run_migrations during setup
