435d2c4998b496ab3a1c6ab5eb2fe49e831c6da3
Facundo Rodriguez <facundonrodriguez@gmail.com>
2025-02-04 09:33:59 -0300

    Bump minor version in src-tauri/tauri.conf.json

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,13 @@ BRANCH_RELEASE="release"
 git checkout $BRANCH_MAIN
 git pull origin $BRANCH_MAIN
 
+# Bump minor version in src-tauri/tauri.conf.json
+jq '.version |= (split(".") | .[1] |= (tonumber + 1 | tostring) | join("."))' src-tauri/tauri.conf.json > tmp-conf.json && mv tmp-conf.json src-tauri/tauri.conf.json
+
+# Commit the version bump
+git add src-tauri/tauri.conf.json
+git commit -m "Bump minor version in src-tauri/tauri.conf.json"
+
 # Merge main into release (fast-forward if possible)
 git checkout $BRANCH_RELEASE
 git pull origin $BRANCH_RELEASE
