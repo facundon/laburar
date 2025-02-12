cfd9a87884c3b65f94b1019defbb6e403915ee48
Facundo Rodriguez <facundonrodriguez@gmail.com>
2025-02-12 18:10:30 -0300

    feat: update release notes and integrate modal for update confirmation

--- src/routes/Updater.svelte
+++ src/routes/Updater.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Modal from '$components/Modal.svelte'
 	import { check, Update } from '@tauri-apps/plugin-updater'
 	import { DownloadCloud } from 'lucide-svelte'
 	import { onDestroy, onMount } from 'svelte'
@@ -17,7 +18,6 @@
 	}
 
 	async function checkUpdate() {
-		console.log('checking for updates')
 		const update = await check()
 		if (!update) return
 		status = 'Se encontró una actualización. Presiona el botón para descargarla.'
@@ -42,6 +42,13 @@
 		})
 	}
 
+	let showModal = $state(false)
+
+	const onConfirm = () => {
+		updateApp()
+		showModal = false
+	}
+
 	let interval = $state<number | null>(null)
 	onMount(() => {
 		checkUpdate()
@@ -54,28 +61,52 @@
 
 {#if status}
 	<div class="update-bar">
-		{#if status}
-			<p>
-				{status}{' '}
-				{#if updater && total === 0}
-					<DownloadCloud
-						onclick={updateApp}
-						color="var(--success-light)"
-						strokeWidth="1"
-						style="margin-left: 0.5rem; vertical-align: middle; cursor: pointer;"
-						size={'1.2rem'}
-					/>
-				{/if}
-				{#if total > 0}
-					<span>{percentage}% ({formatBytes(downloaded)} / {formatBytes(total)})</span>
-				{/if}
-			</p>
-		{/if}
+		<p>
+			{status}{' '}
+			{#if updater && total === 0}
+				<DownloadCloud
+					onclick={() => (showModal = true)}
+					color="var(--success-light)"
+					strokeWidth="1"
+					style="margin-left: 0.5rem; vertical-align: middle; cursor: pointer;"
+					size={'1.2rem'}
+				/>
+			{/if}
+			{#if total > 0}
+				<span>{percentage}% ({formatBytes(downloaded)} / {formatBytes(total)})</span>
+			{/if}
+		</p>
 		<div class="progress" style="width: {percentage}%"></div>
 	</div>
 {/if}
 
+<Modal show={showModal} onconfirm={onConfirm} onclose={() => (showModal = false)} title="Descargar e Instalar version {updater?.version}">
+	<h3>Lista de cambios</h3>
+	{#if updater?.body}
+		<ul>
+			{#each updater.body.split('\n') as line}
+				<li>{line}</li>
+			{/each}
+		</ul>
+	{:else}
+		<p>No hay cambios disponibles.</p>
+	{/if}
+</Modal>
+
 <style>
+	h3 {
+		margin: 0;
+		font-weight: 600;
+	}
+
+	ul {
+		padding-inline-start: 1rem;
+	}
+
+	li {
+		margin-bottom: 0.5rem;
+	}
+
 	.update-bar {
 		margin: auto;
 		background-color: var(--gray-light);
@@ -87,7 +118,7 @@
 		z-index: 1;
 	}
 
-	p {
+	.update-bar p {
 		font-size: 0.8rem;
 		gap: 2rem;
 		color: white;
@@ -95,7 +126,7 @@
 		margin: 0;
 	}
 
-	p > span {
+	.update-bar p > span {
 		font-weight: 600;
 	}
 
